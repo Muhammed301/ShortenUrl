ec7c941cd94f23293b61c6445c50223b257fa18b
ushbig <usbigmohammed@gmail.com>
2023-09-30 11:17:05 +0100

    clean up.

--- app/Http/Controllers/Actions/SaveUrl.php
+++ app/Http/Controllers/Actions/SaveUrl.php
@@ -11,16 +11,12 @@ class SaveUrl
     
     public function execute($validated, $url){
 
-        // if($url){
+        
+        $validated['shorten_url'] =$url->shorten_url ?? ENV('APP_URL').'/'. base_convert($validated['original_url'],16,8);
+        $url->original_url = $validated['original_url'];
+        $url->shorten_url = $validated['shorten_url'];
+        $url->status = $validated['status'] ?? Status::Active;  
 
-        //     $url->original_url = $validated['original_url'];
-
-        // }else{
-            $validated['shorten_url'] =$url->shorten_url ?? ENV('APP_URL').'/'. base_convert($validated['original_url'],16,8);
-            $url->original_url = $validated['original_url'];
-            $url->shorten_url = $validated['shorten_url'];
-            $url->status = $validated['status'] ?? Status::Active;  
-        // }
         $url->save();
         return $url;
     }
